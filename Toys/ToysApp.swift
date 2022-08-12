ce54f7a67d033c8370faf0a91467ac3d66b3957b
Kyeongtae Nam <vituscokr@gmail.com>
2022-08-12 13:21:29 +0900

    Initial Commit

---
+++ Toys/ToysApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToysApp.swift
+//  Toys
+//
+//  Created by Gyeongtae Nam on 2022/08/12.
+//
+
+import SwiftUI
+
+@main
+struct ToysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
